dfc78bb3d8b9cc295ab0d12cdc9104303adf594e
Alastor <cryptosenor@protonmail.com>
2022-11-28 05:38:29 -0500

    yoyo

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import requests
+from bs4 import BeautifulSoup
 
 re = requests.get('https://books.toscrape.com/catalogue/sapiens-a-brief-history-of-humankind_996/index.html')
 res = re.content
+
+soup = BeautifulSoup(res,'html.parser')
+price = soup.find()'p'mp,bredeclase acac ,
